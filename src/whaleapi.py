b4dff34616d17342c0d25685311fa3a000abd760
artemi <artemi13221@gmail.com>
2021-05-12 04:07:07 +0900

    init commit

---
+++ src/whaleapi.py
@@ -0,0 +1,19 @@
+import json
+import requests
+
+class WhaleAPI:
+    api_key = 123
+    url = "https://api.whale-alert.io/v1"
+    status = "status"
+    transactions = "transactions"
+
+    def getStatus(self):
+        pass
+
+    def getTransactions(self, start, end):
+        pass
+
+    def getTransactions(self, start, end, cursor):
+        pass
+
+    
