588b9fce5b84dd41515d5aa74efe483ab4593e52
artemi <artemi13221@gmail.com>
2021-05-12 16:20:40 +0900

    🛠Add check url in whaleapi

--- src/whaleapi.py
+++ src/whaleapi.py
@@ -6,7 +6,22 @@ class WhaleAPI:
     url = "https://api.whale-alert.io/v1"
     status = "status"
     transactions = "transactions"
+    r = requests.get(url)
 
+    def __init__(self):
+        self.errorCheck()
+            
+    def errorCheck(self):
+        if self.r.status_code != 200:
+            print('[ERROR] API server is down. errorcode : ' + str(self.r.status_code))
+        
+        return self.r.status_code
+
+    def connectURL(self, params):
+        self.r = requests(self.url, params=params)
+
+        self.errorCheck()
+            
     def getStatus(self):
         pass
 
@@ -16,4 +31,9 @@ class WhaleAPI:
     def getTransactions(self, start, end, cursor):
         pass
 
+    def getTransactions(self, start, end, cursor, limit):
+        pass
+    
+    def checkStatus(self):
+        pass
     
