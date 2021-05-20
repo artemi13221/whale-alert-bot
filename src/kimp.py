7e9a97aa0ccf73b3d1d7913a222c38f9fd5d446b
artemi <68149389+artemi13221@users.noreply.github.com>
2021-05-20 14:13:30 +0900

    🛠add cal_kimp, get_exchange_rate update get_binance_price, get_upbit_… (#2)
    * 🛠add cal_kimp, get_exchange_rate update get_binance_price, get_upbit_price function
    
    * ⛏update convention

--- src/kimp.py
+++ src/kimp.py
@@ -1,19 +1,21 @@
 import requests
-import json
+import datetime
 
+#exchange_rate : 6H, upbit_price, binance_price : 1m
 class Kimp:
     upbit_api_url = 'https://api.upbit.com/v1/ticker' #param = markets=
     binance_api_url = 'https://api.binance.com/api/v1/ticker/price' #param = symbol
-    exchange_rate_api_url = ''
-    symbols = ['BTC', 'ETH', 'DOGE']
-    requests_data = requests.get()
+    exchange_rate_api_url = 'https://api.ratesapi.io/api/latest?base=USD&symbols=KRW'
+    requests_data = requests.get(upbit_api_url)
 
-    def error_log(self):
+    def error_log(self, msg):
         pass
 
     def error_check(self):
-        if self.requests_data != 200:
-            print('[ERROR] errorcode : ' + str(self.requests_data.status_code))
+        if self.requests_data.status_code != 200:
+            print(str(datetime.date.ctime) + '[ERROR] errorcode : ' + str(self.requests_data.status_code))
+        
+        return self.requests_data.status_code
     
     def connect_url(self, url = '', params = None):
         if params == None:
@@ -21,30 +23,41 @@ class Kimp:
         else:
             self.requests_data = requests.get(url=url, params=params)
         
-        self.error_check()
+        status_code = self.error_check()
+        
+        return status_code
 
-    def get_exchange_rate(self):
-        pass
+    def get_exchange_rate(self): #갱신 주기 6h
+        if (self.connect_url(url=self.exchange_rate_api_url)) == 200:
+            return self.requests_data.json()['rates']['KRW']
+        else:
+            return 0
 
-    def get_binance_price(self, symbol):
+    def get_binance_price(self, symbol='BTC'): #갱신 주기 1m
         symbol = symbol + 'USDT'
         param = {'symbol':symbol}
-        self.connect_url(url=self.binance_api_url, params=param)
-
-        if self.requests_data.status_code == 200:
-            return (int)(self.requests_data.json()['price'])
+        if (self.connect_url(url=self.binance_api_url, params=param)) == 200:
+            return (float)(self.requests_data.json()['price'])
         else:
             return 0
 
-    def get_upbit_price(self, symbol):
+    def get_upbit_price(self, symbol='BTC'): #갱신 주기 1m
         symbol = 'KRW-' + symbol
         param = {'markets':symbol}
-        self.connect_url(url=self.upbit_api_url, params=param)
-
-        if self.requests_data.status_code == 200:
-            return self.requests_data.json()['trade_price']
+        if (self.connect_url(url=self.upbit_api_url, params=param)) == 200:
+            return self.requests_data.json()[0]['trade_price']
         else:
             return 0
 
-    def cal_kimp(self, symbol):
-        pass
+    def cal_kimp(self, exchange_rate=0, binance_price=0, upbit_price=0): #실행 주기 1m
+        result = 0
+        if binance_price == 0 or upbit_price == 0:
+            print(str(datetime.date.ctime) + ' - [ERROR] price data error! check api site or param(symbol).')
+            result = 0
+        elif exchange_rate == 0:
+            print(str(datetime.date.ctime) + ' - [ERROR] exchage rate error! check api site.')
+            result = 0
+        else:
+            result = ((upbit_price / (binance_price * exchange_rate )) * 100) - 100
+        
+        return result
--- src/whaleapi.py
+++ src/whaleapi.py
@@ -1,18 +1,20 @@
-import json
 import requests
 import os
 import time
+import datetime
 
 class WhaleAPI:
-    api_key = os.environ['API_KEY']
+    api_key = os.environ['WHALE_API_KEY']
     url = "https://api.whale-alert.io/v1"
     request_data = requests.get(url)
     now_timestamp = int(time.time())
+    
+    def error_log(self, msg):
+        pass
             
     def error_check(self):
         if self.request_data.status_code != 200:
-            print('[ERROR] errorcode : ' + str(self.request_data.status_code))
-        
+            print(str(datetime.date.ctime) + ' - [ERROR] errorcode : ' + str(self.request_data.status_code)) 
 
     def connect_url(self, addUrl='', params={}):
         param = {'api_key':self.api_key}
