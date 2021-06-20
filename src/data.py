e4de8eb9634712d7c578e27ba03713921da423e8
artemi <artemi13221@gmail.com>
2021-06-20 23:28:21 +0900

    🔨 Add get_data() in data.py

--- src/data.py
+++ src/data.py
@@ -1,5 +1,6 @@
 from whaleapi import WhaleAPI
 import json
+import time
 
 class data:
     #"transaction_type":"transfer" ==> 거래
@@ -8,22 +9,38 @@ class data:
     #usdt의 데이터도 중요함. usdt가 거래소로 이동한다는 것은 무엇인가를 사겠다는 의미할 수 있기 때문임.
     #거래소로 입금 출금 위주의 데이터를 필터링
     whale_api = WhaleAPI
-
-    result = 0
-    count = 0
     raw_data = None
+    result_data = None
 
-    def get_raw_data():
-        pass
+    def check_transaction_type(self, t):
+        if t['transaction_type]'] == "transfer" or t['transaction_type]'] == "burn":
+            return True
+        else:
+            return False
 
-    def check_transaction_type():
-        pass
+    def check_to_wallet(self, t):
+        if t['to']['owner_type'] != 'unknown':
+            return True
+        else:
+            return False
 
-    def check_to_wallet():
-        pass
+    def check_from_wallet(self, t):
+        if t['from']['owner_type'] != 'unknown':
+            return True
+        else:
+            return False
 
-    def check_from_wallet():
-        pass
+    def filtering_data(self):
+        self.result_data = []
+        for t in self.raw_data['transactions']:
+            if self.check_transaction_type and (self.check_from_wallet or self.check_to_wallet):
+                self.result_data.append(t)
 
-    def filtering_data():
-        pass
+    def get_data(self, start, cursor=None, limit=None):
+        self.raw_data = self.whale_api.get_transactions(start=start)
+
+        if self.raw_data['result'] != 'success' :
+            print("[Error] : timestamp : " + str(int(time.time())) + "error code : " + self.raw_data['result'])
+            return
+        else:
+            self.filtering_data()
