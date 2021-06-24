bba1dfd736fd0987106022e8ad356d91db3daa54
artemi <artemi13221@gmail.com>
2021-06-24 11:45:58 +0900

    🚨 Fix compiler error

--- src/data.py
+++ src/data.py
@@ -2,13 +2,13 @@ from whaleapi import WhaleAPI
 import json
 import time
 
-class data:
+class Data:
     #'transaction_type':'transfer' ==> 거래
     #'transaction_type':'burn' ==> 소각
     #unkown -> unkown의 데이터는 불필요.
     #usdt의 데이터도 중요함. usdt가 거래소로 이동한다는 것은 무엇인가를 사겠다는 의미할 수 있기 때문임.
     #거래소로 입금 출금 위주의 데이터를 필터링
-    whale_api = WhaleAPI
+    whale_api = WhaleAPI()
     raw_data = None
     result_data = None
 
